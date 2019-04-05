Delete main, fix bugs in gradient descent

AnnImplementations/MNIST_gradient_descent.py
@@ -4,12 +4,14 @@
 """
 """
 
-import numpy as np;
-import tensorflow as tf;
+import tensorflow as tf
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("MNIST_data/", one_hot=True)
 
-def MNIST_gradient_descent( X_train = None,
+
+def MNIST_gradient_descent( x_train = None,
                             y_train = None,
-                            X_test = None,
+                            x_test = None,
                             y_test = None,
                             learning_rate = 0.05,
                             epochs = 50,
@@ -25,23 +27,21 @@ def MNIST_gradient_descent( X_train = None,
     #~learning_rate = 0.01
 
     ## Function's environment variables
-    update_interval = int ( epochs / 5 );
-    #~total_batches = int( len( mnist.train.labels ) / batch_size );
-    batch_size = int( len( mnist.train.labels ) / batches );
-
+    update_interval = int( epochs / 5 )
+    batch_size = int(len(mnist.train.labels) / batches)
     # Extracting the dimensions of the problem' space.
-    m, n = X_Train.data.shape;
+    m, n = x_train.data.shape
 
     ## Populating the TensorFlow environment
     ## Creating the constants
-    X = tf.constant(    X_Train,
+    x = tf.constant(    x_train,
                         dtype = tf.float32,
-                        name = "X"
-                        );
-    y = tf.constant(    y_Train,
+                        name = "x"
+                        )
+    y = tf.constant(    x_train,
                         dtype = tf.float32,
                         name = "y"
-                        );
+                        )
 
     ## Creating the variables
     theta = tf.Variable(    tf.random_uniform(  [n, 1],
@@ -49,7 +49,7 @@ def MNIST_gradient_descent( X_train = None,
                                                 1.0
                                                 ),
                             name="theta"
-                            );
+                            )
 
     ## Creating the optimizer
     ## calculated using tfs gradient descent optimizer
@@ -58,21 +58,21 @@ def MNIST_gradient_descent( X_train = None,
     ## calculated using tfs momentum descent optimizer - faster
     optimizer = tf.train.MomentumOptimizer( learning_rate = learning_rate,
                                             momentum = 0.9
-                                            );
+                                            )
 
     ## Creating the operations
-    y_pred = tf.matmul( X,
-                        theta,
-                        name="predictions"
-                        );
-    error = y_pred - y;
-    mse = tf.reduce_mean(   tf.square(error),
-                            name="mse"
-                            );
-    training_op = optimizer.minimize(mse);
+    y_pred = tf.matmul(x,
+                       theta,
+                       name="predictions"
+                       )
+    error = y_pred - y
+    mse = tf.reduce_mean(tf.square(error),
+                         name="mse"
+                         )
+    training_op = optimizer.minimize(mse)
 
     ## Initializing the TensorFlow environment
-    init = tf.global_variables_initializer();
+    init = tf.global_variables_initializer()
 
     ## Running the operations between the variables using a TensorFlow.Session.
     ## Class Session: returns TensorFlow operations
@@ -82,13 +82,13 @@ def MNIST_gradient_descent( X_train = None,
     ## the sessions resources when no longer required (using tf.Session.close).
     with tf.Session() as sess:
         ## Completing variables and graph structure initialization.
-        sess.run(init);
+        sess.run(init)
         ## For the requested amount of times (epochs = iterations) train the ANN.
         for epoch in range(epochs):
             avg_cost = 0
-            for batch in range(total_batches):
+            for batch in range(batch_size):
                 batch_x, batch_y = mnist.train.next_batch(batch_size = batch_size)
-                _, cost = sess.run([optimiser, cross_entropy],
+                _, cost = sess.run([optimizer, cross_entropy],
                                 feed_dict = {x: batch_x, y: batch_y})
                 avg_cost += cost / batches
             print("Epoch:", (epoch + 1), "cost =", "{:.3f}".format(avg_cost))
@@ -101,20 +101,28 @@ def MNIST_gradient_descent( X_train = None,
                         epoch,
                         " MSE = ",
                         mse.eval()
-                        );
+                        )
                 ## Compute the output of the graph (run the operations); i.e.
                 ## train the ANN.
-                sess.run(training_op);
+                sess.run(training_op)
         ## Compute and print the final value of the wheights (is thatwhat theta
         ## is?) obtained at the end of the training session (the epochs).
-        best_theta = theta.eval();
+        best_theta = theta.eval()
         print(best_theta)
 
         print(sess.run(accuracy, feed_dict={x: mnist.test.images, y: mnist.test.labels}))
 
 
 if __name__ == "__main__":
-    #~main()
+    MNIST_gradient_descent(x_train=None,
+                           y_train=None,
+                           x_test=None,
+                           y_test=None,
+                           learning_rate=0.05,
+                           epochs=50,
+                           batches=75,
+                           seed=42
+                           )
 
 
 