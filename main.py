78f5ec1b96cf0d221921f3483ea8caa199d053b6
David T. Pocock <davidtpocock@gmail.com>
2019-04-07 17:45:31 +0100

    Add hyperas integration functionality

--- main.py
+++ main.py
@@ -1,27 +1,45 @@
 __author__ = 'Team Alpha'
 
 from imdb import IMDb
-from mnist import MNIST
+from mnist import MNIST, prepare_data, run_first_combo
+from hyperas import optim
+from hyperopt import Trials, tpe
+import keras
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D, Activation
 
-def main():
-    """
-    """
-#    MNIST(
-#        combination=1,
-#        learning_rate=0.05,
-#        epochs=50,
-#        batches=75,
-#        seed=12345
-#    )
 
-    IMDb(
-        combination=1,
-        learning_rate=0.001,
-        epochs=40,
-        batches=500,
-        seed=12345
-    )
+def main(dataset):
+    """if dataset == 0:
+        mnist_model = MNIST(
+            combination=1,
+            learning_rate=0.05,
+            epochs=50,
+            batches=75,
+            seed=12345
+        )
+    elif dataset == 1:
+        imdb_model = IMDb(
+            combination=1,
+            learning_rate=0.001,
+            epochs=40,
+            batches=500,
+            seed=12345
+        )
+        """
+
+    best_run, best_model = optim.minimize(model=run_first_combo,
+                                          data=prepare_data,
+                                          algo=tpe.suggest,
+                                          max_evals=5,
+                                          trials=Trials())
+    x_train, y_train, x_test, y_test = prepare_data()
+    print("Evaluation of best performing model:")
+    print(best_model.evaluate(x_test, y_test))
+    print("Best performing model chosen hyper-parameters:")
+    print(best_run)
 
 
 if __name__ == "__main__":
-    main()
+    main(0)
--- mnist_auto.py
+++ mnist_auto.py
@@ -1,9 +1,7 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 __author__ = 'Team Alpha'
 
-from packageinfo import PackageInfo
+from hyperas import optim
+from hyperopt import Trials, tpe
 from hyperas.distributions import choice, uniform
 from hyperopt import STATUS_OK
 import tensorflow as tf
@@ -13,50 +11,16 @@ from keras.layers import Dense, Conv2D, Dropout, Flatten, MaxPooling2D, Activati
 import numpy as np
 
 
-class MNIST(PackageInfo):
-    def __init__(self, combination, learning_rate, epochs, batches, seed):
-        PackageInfo.__init__(self)
-        self.learning_rate = learning_rate
-        self.epochs = epochs
-        self.batches = batches
-        self.seed = seed
-        self.select_combination(combination)
-
-    def select_combination(self, combination):
-        x_train, y_train, x_test, y_test = prepare_data()
-        if combination == 1:
-            run_first_combo(x_train, y_train, x_test, y_test)
-        elif combination == 2:
-            run_second_combo(x_train, y_train, x_test, y_test)
-        else:
-            raise Exception("Please input 1 or 2 for the combination to run")
-
-    def old_function(self):
-        pass
-
-
-def run_second_combo(x_train, y_train, x_test, y_test):
-    return 0
-
-
 def prepare_data():
     tf.set_random_seed(12345)
     mnist = tf.keras.datasets.mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     x_train = x_train.reshape(x_train.shape[0], 28, 28, 1)
     x_test = x_test.reshape(x_test.shape[0], 28, 28, 1)
-    # Making sure that the values are float so that we can get decimal points after division
     x_train = x_train.astype('float32')
     x_test = x_test.astype('float32')
-    # Normalizing the RGB codes by dividing it to the max RGB value.
     x_train /= 255
     x_test /= 255
-    print()
-    print('x_train shape:', x_train.shape)
-    print('Number of images in x_train', x_train.shape[0])
-    print('Number of images in x_test', x_test.shape[0])
-    print("Shape:", x_train.shape)
-    print()
     return x_train, y_train, x_test, y_test
 
 
@@ -87,3 +51,21 @@ def run_first_combo(x_train, y_train, x_test, y_test):
     validation_acc = np.amax(result.history['val_acc'])
     print('Best validation acc of epoch:', validation_acc)
     return {'loss': -validation_acc, 'status': STATUS_OK, 'model': model}
+
+
+def main():
+    best_run, best_model = optim.minimize(model=run_first_combo,
+                                          data=prepare_data,
+                                          algo=tpe.suggest,
+                                          max_evals=5,
+                                          trials=Trials())
+    x_train, y_train, x_test, y_test = prepare_data()
+    run_first_combo(x_train, y_train, x_test, y_test)
+    print("Evalutation of best performing model:")
+    print(best_model.evaluate(x_test, y_test))
+    print("Best performing model chosen hyper-parameters:")
+    print(best_run)
+
+
+if __name__ == "__main__":
+    main()
