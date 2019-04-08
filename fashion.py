ba921c3afab4d595c1b0338af8e1a17d345c8659
David T. Pocock <davidtpocock@gmail.com>
2019-04-08 19:05:41 +0100

    Store modelname throughout runs

--- fashion.py
+++ fashion.py
@@ -28,14 +28,14 @@ class Fashion(Helper):
         x_train, y_train, x_test, y_test = self.prepare_data()
         if combination == 1:
             model = self.run_first_combo()
-            modelname = "fashion_c1_x_y_z"
+            modelname = "fashion_c1_" + str(self.learning_rate) + "_" + str(self.epochs) + "_" + str(self.batches) + "_" + str(self.seed) + ""
         elif combination == 2:
             model = self.run_second_combo()
-            modelname = "fashion_c2_x_y_z"
+            modelname = "fashion_c2_" + str(self.learning_rate) + "_" + str(self.epochs) + "_" + str(self.batches) + "_" + str(self.seed) + ""
         else:
             raise Exception("Please input 1 or 2 for the combination to run")
         data = x_train, y_train, x_test, y_test
-        result = Helper.fit_and_evaluate(self, model, data, self.batches, self.epochs)
+        result = Helper.fit_and_evaluate(self, model, data, self.batches, self.epochs, modelname)
         Helper.plot_loss_acc(self, result.epoch, result.history['loss'], result.history['acc'],
                              result.history['val_loss'], result.history['val_acc'], modelname)
 
@@ -53,22 +53,16 @@ class Fashion(Helper):
         y_train = np.array(y_train, dtype=np.uint8)
         y_test = np.array(y_test, dtype=np.uint8)
 
-        # x_train.reshape(x_train.shape[0], 28, 28, 1)
-        # x_test.reshape(x_test.shape[0], 28, 28, 1)
-
         if self.combination == 1:
             x_train = np.expand_dims(x_train, axis=-1)
             x_test = np.expand_dims(x_test, axis=-1)
 
         y_train = keras.utils.np_utils.to_categorical(y_train, 10)
         y_test = keras.utils.np_utils.to_categorical(y_test, 10)
-
         x_train = x_train.astype('float32')
         x_test = x_test.astype('float32')
-
         x_train /= 255
         x_test /= 255
-
         return x_train, y_train, x_test, y_test
 
     def run_first_combo(self):
@@ -78,15 +72,15 @@ class Fashion(Helper):
         model.add(BatchNormalization(input_shape=self.input_shape))                 # Normalisation
         model.add(Conv2D(64, (4, 4), padding='same', activation='relu'))            # Convolution
         model.add(MaxPooling2D(pool_size=(2, 2)))                                   # Max Pooling
-        model.add(Dropout(0.1))                                                     # Dropout
-        model.add(Conv2D(64, (4, 4), activation='relu'))                            # Convolution
-        model.add(MaxPooling2D(pool_size=(2, 2)))                                   # Max Pooling
-        model.add(Dropout(0.3))                                                     # Dropout
+        #model.add(Dropout(0.1))                                                     # Dropout
+        #model.add(Conv2D(64, (4, 4), activation='relu'))                            # Convolution
+        #model.add(MaxPooling2D(pool_size=(2, 2)))                                   # Max Pooling
+        #model.add(Dropout(0.3))                                                     # Dropout
         model.add(Flatten())                                                        # Converting 3D feat. to 1D feat.
-        model.add(Dense(256, activation='relu'))                                    # Fully Connected Layer
-        model.add(Dropout(0.5))                                                     # Dropout
-        model.add(Dense(64, activation='relu'))                                     # Fully Connected Layer
-        model.add(BatchNormalization())                                             # Normalization
+        #model.add(Dense(256, activation='relu'))                                    # Fully Connected Layer
+        #model.add(Dropout(0.5))                                                     # Dropout
+        #model.add(Dense(64, activation='relu'))                                     # Fully Connected Layer
+        #model.add(BatchNormalization())                                             # Normalization
         model.add(Dense(self.num_classes, activation='softmax'))
 
         adam = keras.optimizers.Adam(lr=self.learning_rate)                         # default lr=0.001
